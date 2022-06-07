485229f3d256e80e26e11c0c2f9e46de89b8a876
satita-0315 <ss0315_ys@icloud.com>
2022-06-07 23:00:58 +0900

    ビンゴの数字を取得、シャッフルするメソッド修正

--- src/com/example/bingogame/BingoGame.java
+++ src/com/example/bingogame/BingoGame.java
@@ -1,17 +1,30 @@
 package com.example.bingogame;
 
 import java.util.*;
+
 // 3 x 3のビンゴゲーム
 public class BingoGame {
     public static void main(String[] args) {
-        System.out.println("ビンゴゲームを始めます！");
         int[] numbers = makeNumbers();
-        System.out.println(Arrays.toString(numbers));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ビンゴゲームを始めます！(Enterで進みます）");
+        scanner.nextLine();
+        //ゲームスタート
+        for (int n = 0; n < numbers.length; n++) {
+            System.out.println((n + 1) + "回目！");
+            System.out.println("テスト"); //numbers[n]の数字を表示し、変数に格納
+
+            //変数と参加者のシートの数字を比較し、同値のものがあればHiT!
+
+            //１ライン全てHitしたらゲームを抜ける
+
+            scanner.nextLine();
+        }
     }
     // 数字を格納 ひとまず30に設定
     public static int[] makeNumbers() {
         int[] numbers = new int[30];
-        for ( int i = 0; i < numbers.length - 1; i++) {
+        for ( int i = 0; i < numbers.length; i++) {
             numbers[i] = i + 1;
         }
         return numbers;
@@ -19,15 +32,12 @@ public class BingoGame {
     // 格納されている数字をシャッフル
     public static int[] shuffleNumbers(int[] numbers) {
         Random rd = new Random();
-        for (int i = 0; i < numbers.length - 1; i++) {
-            int j = rd.nextInt(i +1);
-
+        for (int i = numbers.length - 1; i > 0; i--) {
+            int j = rd.nextInt(i + 1);
+            int tmp = numbers[j];
+            numbers[j] = numbers[i];
+            numbers[i] = tmp;
         }
-
-//        List<Integer> list = (List<Integer>)Arrays.asList(numbers);
-//        Collections.shuffle(list);
-//        list.toArray(numbers);
-
         return numbers;
     }
 }
