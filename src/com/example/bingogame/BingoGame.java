2933914a65b7777320227951a52679bbe6f658f1
satita-0315 <ss0315_ys@icloud.com>
2022-06-08 11:56:40 +0900

    ビンゴカード、ヒットナンバー生成のメソッドの追加

--- src/com/example/bingogame/BingoGame.java
+++ src/com/example/bingogame/BingoGame.java
@@ -5,16 +5,17 @@ import java.util.*;
 // 3 x 3のビンゴゲーム
 public class BingoGame {
     public static void main(String[] args) {
-        int[] numbers = makeNumbers();
+        int[] hitNumbers = makeHitNumbers();
         Scanner scanner = new Scanner(System.in);
         System.out.println("ビンゴゲームを始めます！(Enterで進みます）");
+        System.out.println(Arrays.deepToString(makeBingoCard(3)));
         scanner.nextLine();
         //ゲームスタート
-        for (int n = 0; n < numbers.length; n++) {
+        for (int n = 0; n < hitNumbers.length; n++) {
             System.out.println((n + 1) + "回目！");
-            System.out.println("テスト"); //numbers[n]の数字を表示し、変数に格納
+            System.out.println(hitNumbers[n]); //hitNumbers[n]の数字を表示し、変数に格納
 
-            //変数と参加者のシートの数字を比較し、同値のものがあればHiT!
+            //hitNumbersと参加者のシートの数字を比較し、同値のものがあればHiT!
 
             //１ライン全てHitしたらゲームを抜ける
 
@@ -40,4 +41,30 @@ public class BingoGame {
         }
         return numbers;
     }
+
+    //メソッドへの引数にint maxNumbersみたいに数字を渡せば、ゲーム初期の設定で参加者がHitNumberの上限値を変更できるようにしたい
+    public static int[] makeHitNumbers() {
+        int[] numbers = shuffleNumbers(makeNumbers());
+        return numbers;
+    }
+
+    public static int[][] makeBingoCard(int width) {
+        int[] numbers = shuffleNumbers(makeNumbers());
+        int [][] card = new int[width][width];
+        ArrayList<Integer> arrayList = new ArrayList<>(numbers.length);
+        //2次元配列をList型に変換
+        for (int n : numbers) {
+            arrayList.add(n);
+        }
+        //ランダムな数字を先頭からcardに格納
+        //continueを用いれば真ん中穴開けられそう
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < width; j++) {
+                card[i][j] = arrayList.get(0);
+                arrayList.remove(0);
+            }
+        }
+        return card;
+    }
+
 }
